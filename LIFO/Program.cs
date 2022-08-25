42f7f7c42733a823148ad49363fd2f8cd9b2dd56
NonWine <zoomguru1@gmail.com>
2022-08-25 21:27:28 +0300

    Stack

---
+++ LIFO/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+class Stack<T>
+{
+    private T[] arr = new T[0];
+    //add an element to the top of the stack
+    public void Append(T val)
+    {
+        T[] newarray = new T[arr.Length + 1];
+        for(int i=0; i < arr.Length; i++)
+        {
+            newarray[i] = arr[i];
+        }
+        newarray[arr.Length] = val;
+        arr = newarray;
+    }
+    //remove an element from the top of the stack
+    public void Delete()
+    {
+        T[] newarray = new T[arr.Length-1];
+        for (int i = 0; i < newarray.Length; i++)
+            newarray[i] = arr[i + 1];
+        arr = newarray;
+    }
+    //show stack
+    public void Show()
+    {
+        for (int i = 0; i < arr.Length; i++)
+            Console.WriteLine(arr[i]);
+    }
+    //Show the top of stack
+    public void ShowTOP()
+    {
+        Console.WriteLine(arr[0]);
+    }
+}
+    class Program
+    {
+        static void Main()
+        {
+        Stack<int> X = new Stack<int>();
+
+        //add elements to stack
+        X.Append(12);
+        X.Append(10);
+       //remove from the top
+        X.Delete();
+       //add to the top
+        X.Append(21312);
+        X.Append(30);
+        //show 
+        Console.Write("Top ");
+        X.ShowTOP();
+        Console.WriteLine("All Stack");
+        X.Show();
+        }
+    }
---
+++ LIFO/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("LIFO")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LIFO")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("50983de8-fcfc-494e-b42d-38125a359ad8")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
